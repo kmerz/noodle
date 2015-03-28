b0238c20959094ca548c5824d7e0cac93dada7dc
Konrad Merz <kmerz@hulud.net>
2015-03-28 08:49:13 +0100

    add gitignroe

--- src/main.hs
+++ src/main.hs
@@ -5,8 +5,6 @@ import qualified Web.Scotty as S
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Text
-import Database.HDBC
-import Database.HDBC.Sqlite3
 
 import qualified Noodle.Views.Index
 
